601f74f5b6f3bc915658cafce06d140abcecd863
swacisko <swatsylwester@gmail.com>
2026-05-15 16:04:23 +0200

    Some additional changes to IHS. Enabled intermittent cycle constraint generation by default :)

--- src/CONTESTS/PACE22/cpsat-experiment/ExpConfig.h
+++ src/CONTESTS/PACE22/cpsat-experiment/ExpConfig.h
@@ -118,7 +118,7 @@ public:
     int cycle_trimming_min_nodes_in_hs = 3;
 
 
-    bool use_ihs_intermittent_cycle_constraints = false;
+    bool use_ihs_intermittent_cycle_constraints = true;
 
     /**
      * This is the value, for which all cycles of at most this length will be created as constraints.
